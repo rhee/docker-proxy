7581592a85d91e490874445f26689dfbb3344d70
Sang-Hoon RHEE <shr386@hotmail.com>
2016-05-09 22:37:50 +0900

    .

--- src/build.sh
+++ src/build.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-
 ( cd polipo-1.1.1 ; sh ../10-build-polipo.sh )
-
 ( cd privoxy-3.0.19-stable ; sh ../20-build-privoxy.sh )
-
 ( cd tor-0.2.2.35 ; sh ../30-build-tor.sh )
-
 ( cd ratproxy-1.58/ratproxy ; sh ../../40-build-ratproxy.sh )
-
 tar cvzf /out/_opt-proxy.tar.gz -C / opt/proxy/bin opt/proxy/etc opt/proxy/sbin opt/proxy/share/tor
