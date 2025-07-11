5414378b27b5391e6b6c6843742cf5564c825baf
Sang-Hoon RHEE <shr386+github@hotmail.com>
2025-07-11 20:58:19 +0900

    .

--- start.sh
+++ start.sh
@@ -100,68 +100,68 @@ nohup /opt/proxy/sbin/privoxy --no-daemon /opt/proxy/etc/privoxy/config &
 
 
 
-echo "Start squid ..." 1>&2
-
-mkdir -p /opt/proxy/etc/squid
-cat<<EOF > /opt/proxy/etc/squid/squid.conf
-acl all src all
-acl manager proto cache_object
-acl localhost src 127.0.0.1/32
-acl localnet src 10.0.0.0/8	# RFC1918 possible internal network
-acl localnet src 172.16.0.0/12	# RFC1918 possible internal network
-acl localnet src 192.168.0.0/16	# RFC1918 possible internal network
-acl SSL_ports port 443		# https
-acl SSL_ports port 563		# snews
-acl SSL_ports port 873		# rsync
-acl Safe_ports port 80		# http
-acl Safe_ports port 21		# ftp
-acl Safe_ports port 443		# https
-acl Safe_ports port 70		# gopher
-acl Safe_ports port 210		# wais
-acl Safe_ports port 1025-65535	# unregistered ports
-acl Safe_ports port 280		# http-mgmt
-acl Safe_ports port 488		# gss-http
-acl Safe_ports port 591		# filemaker
-acl Safe_ports port 777		# multiling http
-acl Safe_ports port 631		# cups
-acl Safe_ports port 873		# rsync
-acl Safe_ports port 901		# SWAT
-acl purge method PURGE
-acl CONNECT method CONNECT
-http_access allow manager localhost
-http_access deny manager
-http_access allow purge localhost
-http_access deny purge
-http_access deny !Safe_ports
-http_access deny CONNECT !SSL_ports
-http_access allow localhost
-http_access allow localnet
-http_access deny all
-icp_access allow localhost
-icp_access allow localnet
-icp_access deny all
-http_port 3128
-access_log /dev/null squid
-refresh_pattern ^ftp:		1440	20%	10080
-refresh_pattern ^gopher:	1440	0%	1440
-refresh_pattern -i (/cgi-bin/|\?) 0	0%	0
-refresh_pattern (Release|Packages(.gz)*)$	0	20%	2880
-refresh_pattern .		0	20%	4320
-acl shoutcast rep_header X-HTTP09-First-Line ^ICY.[0-9]
-acl apache rep_header Server ^Apache
-coredump_dir /var/spool/squid
-### cache_peer 127.0.0.1 parent 8118 0 no-query no-digest
-never_direct allow all
-EOF
-
-## obsolete
-#upgrade_http0.9 deny shoutcast
-#broken_vary_encoding allow apache
-#extension_methods REPORT MERGE MKACTIVITY CHECKOUT
-#hierarchy_stoplist cgi-bin ?
-#acl to_localhost dst 127.0.0.0/8 0.0.0.0/32
-#hosts_file /etc/hosts
-
-nohup bash -c 'while :; do /usr/sbin/squid -N -f /opt/proxy/etc/squid/squid.conf -a 3128 -u 3130; sleep 30; done' &
+# echo "Start squid ..." 1>&2
+# 
+# mkdir -p /opt/proxy/etc/squid
+# cat<<EOF > /opt/proxy/etc/squid/squid.conf
+# acl all src all
+# acl manager proto cache_object
+# acl localhost src 127.0.0.1/32
+# acl localnet src 10.0.0.0/8	# RFC1918 possible internal network
+# acl localnet src 172.16.0.0/12	# RFC1918 possible internal network
+# acl localnet src 192.168.0.0/16	# RFC1918 possible internal network
+# acl SSL_ports port 443		# https
+# acl SSL_ports port 563		# snews
+# acl SSL_ports port 873		# rsync
+# acl Safe_ports port 80		# http
+# acl Safe_ports port 21		# ftp
+# acl Safe_ports port 443		# https
+# acl Safe_ports port 70		# gopher
+# acl Safe_ports port 210		# wais
+# acl Safe_ports port 1025-65535	# unregistered ports
+# acl Safe_ports port 280		# http-mgmt
+# acl Safe_ports port 488		# gss-http
+# acl Safe_ports port 591		# filemaker
+# acl Safe_ports port 777		# multiling http
+# acl Safe_ports port 631		# cups
+# acl Safe_ports port 873		# rsync
+# acl Safe_ports port 901		# SWAT
+# acl purge method PURGE
+# acl CONNECT method CONNECT
+# http_access allow manager localhost
+# http_access deny manager
+# http_access allow purge localhost
+# http_access deny purge
+# http_access deny !Safe_ports
+# http_access deny CONNECT !SSL_ports
+# http_access allow localhost
+# http_access allow localnet
+# http_access deny all
+# icp_access allow localhost
+# icp_access allow localnet
+# icp_access deny all
+# http_port 3128
+# access_log /dev/null squid
+# refresh_pattern ^ftp:		1440	20%	10080
+# refresh_pattern ^gopher:	1440	0%	1440
+# refresh_pattern -i (/cgi-bin/|\?) 0	0%	0
+# refresh_pattern (Release|Packages(.gz)*)$	0	20%	2880
+# refresh_pattern .		0	20%	4320
+# acl shoutcast rep_header X-HTTP09-First-Line ^ICY.[0-9]
+# acl apache rep_header Server ^Apache
+# coredump_dir /var/spool/squid
+# ### cache_peer 127.0.0.1 parent 8118 0 no-query no-digest
+# never_direct allow all
+# EOF
+# 
+# ## obsolete
+# #upgrade_http0.9 deny shoutcast
+# #broken_vary_encoding allow apache
+# #extension_methods REPORT MERGE MKACTIVITY CHECKOUT
+# #hierarchy_stoplist cgi-bin ?
+# #acl to_localhost dst 127.0.0.0/8 0.0.0.0/32
+# #hosts_file /etc/hosts
+# 
+# nohup bash -c 'while :; do /usr/sbin/squid -N -f /opt/proxy/etc/squid/squid.conf -a 3128 -u 3130; sleep 30; done' &
 
 tail -F /--nothing--
